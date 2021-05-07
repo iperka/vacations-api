103326a4910870ff7b377085c14477c2ab50859c
Michael Beutler <michael.beutler@iperka.com>
2021-05-07 15:22:40 +0200

    Updated `User` model. 🙍‍♂️
    Added some new fields for authentication.
    + Generated Getters and Setters with VSCode 👩‍💻

--- src/main/java/com/iperka/vacations/api/users/User.java
+++ src/main/java/com/iperka/vacations/api/users/User.java
@@ -21,8 +21,8 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
  * authorization and specification of user details and states.
  * 
  * @author Michael Beutler
- * @version 0.0.1
- * @since 2021-07-05
+ * @version 0.0.2
+ * @since 2021-05-07
  */
 @Entity
 @Table(name = "users")
@@ -58,6 +58,36 @@ public class User {
     @NotNull
     @JsonIgnore
     private String password;
+
+    /**
+     * Will be false if the user is marked as disabled. As default every user
+     * account is disabled and needs to be enabled in order to function as
+     * authorization object.
+     */
+    @Column(nullable = false)
+    @NotNull
+    private boolean enabled = false;
+
+    /**
+     * Will be true if the user is marked as locked.
+     */
+    @Column(nullable = false)
+    @NotNull
+    private boolean locked = false;
+
+    /**
+     * Will be true if the user is marked as expired.
+     */
+    @Column(nullable = false)
+    @NotNull
+    private boolean expired = false;
+
+    /**
+     * Will be true if the user credentials is marked as expired.
+     */
+    @Column(nullable = false)
+    @NotNull
+    private boolean credentialsExpired = false;
 
     /**
      * Stores the date time when the user object has been created.
@@ -99,5 +129,77 @@ public class User {
     public User(@Min(4) @Max(100) @NotNull String username, @NotNull String password) {
         this.username = username;
         this.password = password;
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public boolean isLocked() {
+        return locked;
+    }
+
+    public void setLocked(boolean locked) {
+        this.locked = locked;
+    }
+
+    public boolean isExpired() {
+        return expired;
+    }
+
+    public void setExpired(boolean expired) {
+        this.expired = expired;
+    }
+
+    public boolean isCredentialsExpired() {
+        return credentialsExpired;
+    }
+
+    public void setCredentialsExpired(boolean credentialsExpired) {
+        this.credentialsExpired = credentialsExpired;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public Date getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(Date updated) {
+        this.updated = updated;
     }
 }
