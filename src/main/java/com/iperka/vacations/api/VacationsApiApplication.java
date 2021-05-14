35e3f56307cbd4fa5113d20fa9bb3c919adb1da5
Michael Beutler <michael.beutler@iperka.com>
2021-05-14 16:28:20 +0200

    Added `Logger` to main method. 🎃

--- src/main/java/com/iperka/vacations/api/VacationsApiApplication.java
+++ src/main/java/com/iperka/vacations/api/VacationsApiApplication.java
@@ -1,12 +1,17 @@
 package com.iperka.vacations.api;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VacationsApiApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(VacationsApiApplication.class);
+
 	public static void main(String[] args) {
+		logger.info("Starting API...");
 		SpringApplication.run(VacationsApiApplication.class, args);
 	}
 
