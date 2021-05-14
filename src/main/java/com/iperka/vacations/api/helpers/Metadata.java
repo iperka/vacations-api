5cf09916d9bc86531a7a096577c72c3a88abccec
Michael Beutler <michael.beutler@iperka.com>
2021-05-14 11:27:38 +0200

    Added basic serialization helpers. 🛠

---
+++ src/main/java/com/iperka/vacations/api/helpers/Metadata.java
@@ -0,0 +1,79 @@
+package com.iperka.vacations.api.helpers;
+
+import org.springframework.data.domain.Page;
+
+/**
+ * The {@link com.iperka.vacations.api.helpers.Metadata} class defines the
+ * structure of a metadata object. This object is mostly used for
+ * {@link org.springframework.data.domain.Page} objects and describe their
+ * metadata. Use the special constructor to create a object from page object.
+ * 
+ * Will be serialized as:
+ * <code>
+ * "metadata": {
+ *   "totalElements": 0,
+ *   "totalPages": 0,
+ *   "page": 1,
+ *   "perPage": 20
+ * }
+ * </code>
+ * 
+ * @author Michael Beutler
+ * @version 0.0.1
+ * @since 2021-05-14
+ */
+public class Metadata {
+    private long totalElements = 0;
+    private long totalPages = 0;
+    private long page = 1;
+    private long perPage = 0;
+
+    public Metadata(long totalElements, long totalPages, long page, long perPage) {
+        this.totalElements = totalElements;
+        this.totalPages = totalPages;
+        this.page = page;
+        this.perPage = perPage;
+    }
+
+    public Metadata(Page<?> page) {
+        this.totalElements = page.getTotalElements();
+        this.totalPages = page.getTotalPages();
+
+        // Add one to slice number to achieve page 1
+        this.page = page.getNumber() + 1;
+        this.perPage = page.getSize();
+    }
+
+    public long getTotalElements() {
+        return totalElements;
+    }
+
+    public void setTotalElements(long totalElements) {
+        this.totalElements = totalElements;
+    }
+
+    public long getTotalPages() {
+        return totalPages;
+    }
+
+    public void setTotalPages(long totalPages) {
+        this.totalPages = totalPages;
+    }
+
+    public long getPage() {
+        return page;
+    }
+
+    public void setPage(long page) {
+        this.page = page;
+    }
+
+    public long getPerPage() {
+        return perPage;
+    }
+
+    public void setPerPage(long perPage) {
+        this.perPage = perPage;
+    }
+
+}
---
+++ src/main/java/com/iperka/vacations/api/helpers/Response.java
@@ -0,0 +1,110 @@
+package com.iperka.vacations.api.helpers;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+/**
+ * The {@link com.iperka.vacations.api.helpers.Response} class defines the
+ * default structure for responses. It includes the status, a message,
+ * timestamp, version string, data and metadata. This class is a helper function
+ * and to get a {@link org.springframework.http.ResponseEntity} call
+ * <code>build()</code>. The data field is generic and can be passed within the
+ * <>. Some often used use cases are implemented as shortcut method. Like
+ * <code>fromPage()</code>. The setter methods are designed to be chained and
+ * return the object itself.
+ * 
+ * @author Michael Beutler
+ * @version 0.0.1
+ * @since 2021-05-14
+ */
+public class Response<T> {
+    private HttpStatus status;
+    private String message;
+    private String version = "v0.0.1";
+    private final Date timestamp = new Date();
+    private T data = null;
+    private Metadata metadata = null;
+
+    public Response(HttpStatus status, String message, String version, T data, Metadata metadata) {
+        this.status = status;
+        this.message = message;
+        this.version = version;
+        this.data = data;
+        this.metadata = metadata;
+    }
+
+    public Response(HttpStatus status) {
+        this.status = status;
+    }
+
+    public static <T> Response<List<T>> fromPage(HttpStatus status, Page<T> page) {
+        Response<List<T>> response = new Response<List<T>>(status);
+        response.data = page.getContent();
+        response.metadata = new Metadata(page);
+
+        return response;
+    }
+
+    public ResponseEntity<Response<T>> build() {
+        // Apply reason phrase to message field if not set
+        if (this.message == null) {
+            this.message = status.getReasonPhrase();
+        }
+
+        return ResponseEntity.status(status).body(this);
+    }
+
+    public int getStatus() {
+        return status.value();
+    }
+
+    public Response<T> setStatus(HttpStatus status) {
+        this.status = status;
+        return this;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public Response<T> setMessage(String message) {
+        this.message = message;
+        return this;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public Response<T> setVersion(String version) {
+        this.version = version;
+        return this;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public Response<T> setData(T data) {
+        this.data = data;
+        return this;
+    }
+
+    public Metadata getMetadata() {
+        return metadata;
+    }
+
+    public Response<T> setMetadata(Metadata metadata) {
+        this.metadata = metadata;
+        return this;
+    }
+
+}
