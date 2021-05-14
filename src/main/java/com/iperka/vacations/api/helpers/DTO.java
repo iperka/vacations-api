38b950bb8ce50be54f880f4844e8ca56f99cadbf
Michael Beutler <michael.beutler@iperka.com>
2021-05-14 11:26:51 +0200

    Added java doc comments to interface. 📚

--- src/main/java/com/iperka/vacations/api/helpers/DTO.java
+++ src/main/java/com/iperka/vacations/api/helpers/DTO.java
@@ -1,5 +1,18 @@
 package com.iperka.vacations.api.helpers;
 
+/**
+ * Very basic DTO interface. Implement this interface with each DTO class.
+ * 
+ * @author Michael Beutler
+ * @version 1.0.0
+ * @since 2021-05-14
+ */
 public interface DTO<T> {
+
+    /**
+     * Get entity object from DTO.
+     * 
+     * @return Entity.
+     */
     public T toObject();
 }
