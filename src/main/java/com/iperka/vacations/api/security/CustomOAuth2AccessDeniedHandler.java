208ec0cde57f244c3e21b0b65e22bd3f5d83c2d8
Michael Beutler <michael.beutler@iperka.com>
2021-09-29 10:31:12 +0200

    Added basic `Resource Server` implementation. 🔐

---
+++ src/main/java/com/iperka/vacations/api/security/CustomOAuth2AccessDeniedHandler.java
@@ -0,0 +1,50 @@
+package com.iperka.vacations.api.security;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.iperka.vacations.api.helpers.Response;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.oauth2.server.resource.authentication.AbstractOAuth2TokenAuthenticationToken;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+/**
+ * The {@link com.iperka.vacations.api.security.CustomOAuth2AccessDeniedHandler}
+ * class handels requests with insufficient scope privileges.
+ * 
+ * TODO: Extend Error message object.
+ * 
+ * @author Michael Beutler
+ * @version 0.0.1
+ * @since 2021-09-29
+ */
+public class CustomOAuth2AccessDeniedHandler implements AccessDeniedHandler {
+    public static final Logger logger = LoggerFactory.getLogger(CustomOAuth2AccessDeniedHandler.class);
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException e)
+            throws IOException {
+        logger.error(e.getLocalizedMessage(), e);
+
+        String errorMessage = e.getLocalizedMessage();
+
+        if (request.getUserPrincipal() instanceof AbstractOAuth2TokenAuthenticationToken) {
+            errorMessage = "The request requires higher privileges than provided by the access token.";
+        }
+
+        ObjectMapper mapper = new ObjectMapper();
+        Response<?> responseObject = new Response<>(HttpStatus.FORBIDDEN);
+
+        response.setStatus(HttpStatus.FORBIDDEN.value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.getWriter().write(mapper.writeValueAsString(responseObject));
+    }
+}
---
+++ src/main/java/com/iperka/vacations/api/security/CustomOAuth2AuthenticationEntryPoint.java
@@ -0,0 +1,45 @@
+package com.iperka.vacations.api.security;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.iperka.vacations.api.helpers.Response;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+/**
+ * The {@link com.iperka.vacations.api.security.CustomOAuth2AccessDeniedHandler}
+ * class handels requests with insufficient authentication.
+ * 
+ * TODO: Extend Error message object.
+ * 
+ * @author Michael Beutler
+ * @version 0.0.1
+ * @since 2021-09-29
+ */
+public class CustomOAuth2AuthenticationEntryPoint implements AuthenticationEntryPoint {
+    private static final Logger logger = LoggerFactory.getLogger(CustomOAuth2AuthenticationEntryPoint.class);
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException e)
+            throws IOException {
+        logger.error(e.getLocalizedMessage(), e);
+
+        String errorMessage = "Insufficient authentication details";
+
+        ObjectMapper mapper = new ObjectMapper();
+        Response<?> responseObject = new Response<>(HttpStatus.UNAUTHORIZED);
+
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.getWriter().write(mapper.writeValueAsString(responseObject));
+    }
+}
