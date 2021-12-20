6c7c080340490f7e143bb34130d78bcd629d7e22
Michael Beutler <michael.beutler@iperka.com>
2021-12-20 17:39:08 +0100

    Removed unused imports. 🥁

--- src/main/java/com/iperka/vacations/api/helpers/HelperController.java
+++ src/main/java/com/iperka/vacations/api/helpers/HelperController.java
@@ -1,17 +1,13 @@
 package com.iperka.vacations.api.helpers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import java.io.IOException;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
-import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/")
